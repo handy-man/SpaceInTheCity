122c519fdbbcf3af90ae090bb683a1d4060a3045
Nathan Hand <administrator@thehiddennation.com>
2014-08-08 15:14:55 +0100

    password changing for users.

---
+++ public_html/home/admin-pass.php
@@ -0,0 +1,108 @@
+<?PHP require('adminonly.php');
+require('../config.php');
+require('../core/navigation.php');
+require('../core/functions.php');
+require('../dbconfig.php');
+
+if (isset($_COOKIE['passwordchanged'])){
+$newuserset = true;
+setcookie("passwordchanged", "true", time()-3600, '/');
+}
+
+if (isset($_COOKIE['mismatch'])){
+$mismatch = true;
+setcookie("mismatch", "true", time()-3600, '/');
+}
+
+if (isset($_COOKIE['noexist'])){
+$noexist = true;
+setcookie("noexist", "true", time()-3600, '/');
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+<?PHP include($home . "/includes/meta.html");  ?> 
+
+    <title>Create a new user</title>
+	
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/navbar-fixed-top.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+	
+    <!-- Fixed navbar repeated code because we need to change active page. -->
+	<div id="wrap">
+    <?PHP
+	Navigation_home($home);
+	?>
+    <div class="container">
+	<?PHP
+		if($newuserset == true){
+			echo "<div style='text-align: center; margin: auto;' class='alert alert-success fade in hints'>Success new password set!<a class='close' data-dismiss='alert' href='#' aria-hidden='true'>&times;</a> </div>";
+		}
+		
+		if($mismatch == true){
+			echo "<div style='text-align: center; margin: auto;' class='alert alert-danger fade in hints'>Passwords didn't match!<a class='close' data-dismiss='alert' href='#' aria-hidden='true'>&times;</a> </div>";
+		}
+		
+				if($noexist == true){
+			echo "<div style='text-align: center; margin: auto;' class='alert alert-danger fade in hints'>User doesn't exist!<a class='close' data-dismiss='alert' href='#' aria-hidden='true'>&times;</a> </div>";
+		}
+	
+	?>
+	<div class="page-header">
+		<h1>Password manager  <small>Set a user password</small></h1>
+	</div>
+      <!-- Main component for a primary marketing message or call to action -->
+
+	  
+		<form class="form-signin" name="form1" role="form" method="post" action="./changepassword.php">
+		<div class="input-group input-group-sm">
+		<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+        <input name="displayname" id="displayname" data-toggle="tooltip" data-placement="right" title="" data-original-title="Displayname" type="text" class="form-control" placeholder="Display Name" required>
+		</div>
+		<div class="input-group input-group-sm">
+		<span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+        <input name="password" id="password" data-toggle="tooltip" data-placement="right" title="" data-original-title="Password" type="password" class="form-control" placeholder="Password" required>
+		</div>
+		<div class="input-group input-group-sm">
+		<span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+		<input name="conf_password" id="conf_password" type="password" class="form-control" placeholder="Confirm Password" required>
+		</div>
+		<div  style="margin-bottom: 10px;"></div>
+		
+		<button class='btn btn-lg btn-primary btn-block' type='submit'>Change password for the user</button>
+		
+      </form>
+
+    </div> <!-- /container -->
+	
+	</div><!-- /wrap -->
+	<?PHP include("../includes/footer.php");?>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+	
+		    <script>
+	$(document).ready(function() {
+	$('#displayname').tooltip({'trigger':'focus', 'title': 'tooptip'});
+	$('#email').tooltip({'trigger':'focus', 'title': 'tooptip'});
+	$('#password').tooltip({'trigger':'focus', 'title': 'tooptip'});
+	});
+	</script>
+  </body>
+</html>
---
+++ public_html/home/changepassword.php
@@ -0,0 +1,49 @@
+<?PHP
+//Load our database
+require("../dbconfig.php");
+require("../config.php");
+require("../includes/session.php");
+require("../includes/settings.php");
+require("../core/functions.php");
+
+//Database connect
+$connect = mysqli_connect($host,$user,$pass,$dbname);
+//Username & password grab
+$displayname = $_POST['displayname'];
+$password = $_POST['password'];
+$password_conf = $_POST['conf_password'];
+$hash = md5( rand(0,1000) );
+
+$displayname = mysqli_real_escape_string($connect, $displayname);
+$password = mysqli_real_escape_string($connect, $password);
+$password_conf = mysqli_real_escape_string($connect, $password_conf);
+
+$check = mysqli_query($connect, "SELECT * FROM `users` WHERE `username` = '$displayname' LIMIT 0, 30 ");	
+$result = mysqli_num_rows($check);
+
+
+if($password != $password_conf){
+//redirect back to index with error
+setcookie("mismatch", "true", time()+60, '/');
+header('Location:' . $home . '/home/admin-pass.php');
+exit();
+}
+else if($result == 1){
+//carry on
+$displayname = mysqli_real_escape_string($connect, $displayname);
+$password = mysqli_real_escape_string($connect, $password);
+$salt_part1 = md5( rand(0,1000) );
+$salt_part2 = md5( rand(0,1000) );
+$salt = crypt($salt_part1, $salt_part2);
+$password = crypt($password, $salt);
+setcookie("passwordchanged", "true", time()+60, '/');
+$new_user = mysqli_query($connect, "UPDATE `users` SET `password` = '$password', `salt` = '$salt' WHERE `username` = '$displayname'");
+header('Location:' . $home . '/home/admin-pass.php');
+}
+else{
+setcookie("noexist", "true", time()+60, '/');
+header('Location:' . $home . '/home/admin-pass.php');
+
+}
+?>
+
