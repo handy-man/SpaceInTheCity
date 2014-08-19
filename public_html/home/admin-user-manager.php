dc21c7b9b55c69f236dbdbda77a4997bc3867f8f
Nathan Hand <administrator@thehiddennation.com>
2014-08-19 12:00:47 +0100

    Lets not give sani (or whoever) to enable and disable admin accounts.

--- public_html/home/admin-user-manager.php
+++ public_html/home/admin-user-manager.php
@@ -84,7 +84,7 @@ $noadmins = true;
 		<select name="admin_email" class="form-control">
 		<?PHP
 	
-		$userlist = mysqli_query($connect, "SELECT `ID`, `username` FROM `users` ORDER BY `username` ASC");
+		$userlist = mysqli_query($connect, "SELECT `ID`, `username` FROM `users` WHERE `level` != '2' ORDER BY `username` ASC");
 		while($userlistprint = mysqli_fetch_array($userlist, MYSQLI_ASSOC)) {
 		echo "<option value='" . $userlistprint['username'] . "'>" . $userlistprint['username'] . "</option>";
 		}	
